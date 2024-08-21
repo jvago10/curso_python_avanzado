ec57dd6f657956b1507e516f40d45b453182c37d
José Vargas Gonzales <jose.vargas@intertech.network>
2024-08-21 17:44:56 -0500

    culminacion de clase 4

--- modulo2/clase4/main.py
+++ modulo2/clase4/main.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+import requests
+import json
+
+BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
+
+
+def get_pokemon_info(pokemon_name):
+    api_url = BASE_URL + pokemon_name
+    response = requests.get(api_url)
+
+    try:
+        pokemon_data = json.loads(response.text)
+        print(pokemon_data)
+
+    except ValueError:
+        print('No se encontró el pokemon')
+
+
+if __name__ == '__main__':
+    search_pokemon_name = input('Ingrese el nombre del pokemon: ')
+    get_pokemon_info(search_pokemon_name)
